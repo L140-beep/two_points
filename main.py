5e6a0beef7aabec022753d7e20a0263bdfc34a3d
L140-beep <kartoshka0302@mail.ru>
2022-11-29 16:51:46 +0800

    removed comments

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def recvall(sock, n):
         
     return data
 
-# plt.ion()
-# plt.figure()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as suck:
     suck.connect((host, port))
     count = 0
@@ -37,12 +36,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as suck:
                             dtype='uint8').reshape(rows, cols)
         
         t_image = image.copy()
-        #t_image = np.mean(t_image, 2)
         t_image[t_image > 0] = 1
         t_image = label(t_image)
-        # plt.imshow(t_image)
         regions = regionprops(t_image)
-        # print(regions[0]["centroid"])
         
         dots_count = t_image.max()
         
@@ -51,27 +47,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as suck:
         
         pos1 = regions[0]["centroid"]
         pos2 = regions[1]["centroid"]
-        
-        # t_image[t_image == t_image[t_pos1]] = 0
-        
-        # #plt.pause(5)
-        
-        # t_pos2 = np.unravel_index(np.argmax(t_image), t_image.shape)
-        print(pos1, pos2)
-        # print(t_pos1, t_pos2)
-        
-        # plt.clf()
-        # plt.imshow(image)
-        # plt.pause(40)
+
         result = np.round(np.sqrt((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2), 1)
         print(result)
         suck.send(f"{result}".encode())
         validation = suck.recv(20)
         print(validation)
         
-        # suck.send(b"beat")
-        # beat = suck.recv(20)
-        # print(beat)
         if(validation == b"yep"):
             count += 1
         
